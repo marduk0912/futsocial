5d22a0485414f737138a5ff95b78a5887dcbe9f2
Fernando Salvador <fsalvador.gestion@gmail.com>
2020-03-22 15:18:21 -0300

    Initial Commit

---
+++ futSocial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  futSocial
+//
+//  Created by Fernando on 22/03/2020.
+//  Copyright © 2020 Fernando Salvador. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
