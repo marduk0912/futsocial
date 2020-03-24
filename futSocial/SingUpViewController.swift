aec178b1df768ba9fb4cedf8a5cc365c4003f298
Fernando Salvador <fsalvador.gestion@gmail.com>
2020-03-23 21:19:58 -0300

    Ordenando proyecto

---
+++ futSocial/SingUpViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SingUpViewController.swift
+//  futSocial
+//
+//  Created by Fernando on 22/03/2020.
+//  Copyright © 2020 Fernando Salvador. All rights reserved.
+//
+
+import UIKit
+
+class SingUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func dismissLogUp(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    
+
+}
---
+++ futSocial/UserViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UsuarioViewController.swift
+//  futSocial
+//
+//  Created by Fernando on 23/03/2020.
+//  Copyright © 2020 Fernando Salvador. All rights reserved.
+//
+
+import UIKit
+
+class UsuarioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
